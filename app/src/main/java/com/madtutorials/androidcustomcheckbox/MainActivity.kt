b2582db28d659fcd5d91ccfd66ff8ee2dc325d7d
Blackz Shaik <blackzshaik@gmail.com>
2020-06-03 08:18:39 +0530

    adding multiple check boxes

--- app/src/main/java/com/madtutorials/androidcustomcheckbox/MainActivity.kt
+++ app/src/main/java/com/madtutorials/androidcustomcheckbox/MainActivity.kt
@@ -7,26 +7,38 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     
-    private lateinit var normalCheckBox:CheckBox
+    private lateinit var checkBox1: CheckBox
+    private lateinit var checkBox2:CheckBox
     private lateinit var customCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        normalCheckBox = findViewById(R.id.normalCheckBox)
+        checkBox1 = findViewById(R.id.normalCheckBox)
+        checkBox2 = findViewById(R.id.normalCheckBox2)
         customCheckBox = findViewById(R.id.customCheckBox)
 
-        normalCheckBox.setOnCheckedChangeListener { _, isChecked ->
+        checkBox1.setOnCheckedChangeListener { _, isChecked ->
             if(isChecked){
-                Toast.makeText(applicationContext,"Normal Checkbox Checked",Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext,"Checkbox One Checked",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext,"Checkbox One Unchecked",Toast.LENGTH_SHORT).show()
+            }
+        }
+        checkBox2.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                Toast.makeText(applicationContext,"Checkbox Two Checked",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext,"Checkbox Two Unchecked",Toast.LENGTH_SHORT).show()
             }
         }
 
         customCheckBox.setOnCheckedChangeListener{ _,isChecked ->
-            if(isChecked) Toast.makeText(applicationContext,"Custom Checkbox Checked",Toast.LENGTH_SHORT)
-                .show()
+            val text = if(isChecked) "Checkbox Three Checked"
+                       else
+                       "Checkbox Three Unchecked"
+            Toast.makeText(applicationContext,text,Toast.LENGTH_SHORT).show()
         }
-
 
     }
 }
